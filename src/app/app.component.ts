a0d18c47db6b358f315fe181f98edec69c4e3454
Adriano Gonçalves de Resende <adrianogdr@gmail.com>
2022-09-07 08:21:43 -0300

    First Commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,22 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
+
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
+  url:string = 'assets/img/logopbhdadosabertos.png';
   title = 'pda-hm';
+  constructor(){
+  }
+  ngOnInit(): void {
+
+  }
 }
+
+/* git remote add origin https://github.com/adrianogdr/PDA.git
+git branch -M main
+git push -u origin main
+  */
